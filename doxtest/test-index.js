f16a362fd0910f1205dcc3f03d416c9c2a824129
youxiachai <youxiachai@gmail.com>
2014-04-22 04:03:02 +0800

    update doc

--- doxtest/test-index.js
+++ doxtest/test-index.js
@@ -4,14 +4,19 @@
 
 
 var swagger  = require('../lib/dox-swagger'),
+    path = require('path'),
     fs  = require('fs');
 
-var files = fs.readdirSync('./test-code');
+var basePath = path.resolve('.');
+
+var codeDir = path.join(basePath, 'test-code');
+
+var files = fs.readdirSync(codeDir);
 console.log(files)
 
 files.forEach(function (item) {
 
-    swagger.doxSwagger('./test-code/' + item, {basePath : 'http://localhost:5000'});
+    swagger.doxSwagger(path.join(codeDir, item), {basePath : 'http://localhost:5000'});
 })
 
 //swagger.doxSwagger('test-code/user.js', {basePath : 'http://localhost:5000'});
